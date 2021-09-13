25b7d83f408e6983c9c113daaf603f870a644a92
Cody Herbst <codyherbst@Codys-MacBook-Pro.local>
2021-09-13 12:10:16 -0400

    Updates

---
+++ precache-manifest.f188b553188a253fd2304b6fb85e6ff8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebe8c56d6d2f5392dab04322044bcea7",
+    "url": "/Blog/index.html"
+  },
+  {
+    "revision": "653ffd00ead2f9a788e8",
+    "url": "/Blog/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "8ecadf7a78a180e3efcc",
+    "url": "/Blog/static/css/main.93cea29f.chunk.css"
+  },
+  {
+    "revision": "653ffd00ead2f9a788e8",
+    "url": "/Blog/static/js/2.c7a926ee.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Blog/static/js/2.c7a926ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ecadf7a78a180e3efcc",
+    "url": "/Blog/static/js/main.c78bb283.chunk.js"
+  },
+  {
+    "revision": "5f0d19db11a5d9b64bb5",
+    "url": "/Blog/static/js/runtime-main.2f500054.js"
+  }
+]);
